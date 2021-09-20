8d593407906694b9cde4d506db3dd4c77c98bb1e
ouwen000 <wenhong520@qq.com>
2021-09-20 22:58:22 +0800

    1

---
+++ 111111111/main.lua
@@ -0,0 +1,482 @@
+-- 此处为nLog功能实现操作
+nLog = require('nLog')()
+require('xxtsp')
+
+
+--arr = {2222222}
+--arr.www = 12312312312
+--arr.ffff = 'fdsfsdafas'
+--for var=1,6 do
+--	table.insert(arr,2,var)
+--end
+
+
+--nLog(arr)
+
+--for key , var in pairs(arr) do
+--	nLog(key,var)
+--end	
+
+  
+
+
+
+
+--for key, var in pairs(ww) do
+	
+--	nLog(key,var)
+	
+--end	
+
+
+
+
+
+--function table_maxn(t)
+--  local mn=nil;
+--  for k, v in pairs(t) do
+--    if(mn==nil) then
+--      mn=v
+--    end
+--    if mn < v then
+--      mn = v
+--    end
+--  end
+--  return mn
+--end
+--tbl = {[1] = 2, [2] = 6, [3] = 34, ["wen"] =5}
+--nLog("tbl 最大值：", table_maxn(tbl))
+--nLog("tbl 长度 ", #tbl)
+
+
+--for key,vluve in pairs(tbl) do
+	
+--	nLog(变量1)
+
+
+----end
+
+
+--People = {}
+--function clone(tab)
+--local ins = {}
+--	for key , var in pairs(tab) do
+--		ins[key] = var
+--	end
+--	return ins
+--end
+
+--People.sayhi = function ()
+--	nLog("People sayhi")
+--end
+
+
+--local p = clone(People)
+
+--p.sayhi()
+
+
+
+
+----设置 = "com.apple.Preferences"
+	
+----itunesid = {{
+----	{ 43, 710, 0x1bacf8},
+----	{ 39, 726, 0xc5e3fc},
+----	{ 42, 736, 0xc4dffc},
+----	{ 72, 737, 0xe1eefe},
+----	{ 78, 724, 0x1c95f6},
+----	{510, 721, 0x000000},
+----	{509, 729, 0x000000},
+----}, 85, 0, 0, 0, 0}
+
+
+--登录 = {{{409, 442, 0xf9f9f9},}, 85}
+--	input('a@gemehouse.com')
+--	delay(3)	
+--	click(96, 387)
+--	input('AaDd112211')
+--	delay(3)
+--	d(登录,"登录",true,1)
+
+
+--[[]]
+--1-10
+	if device.name() == "0001" then
+		yhnames = '80hb1'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "0003" then
+		yhnames = '80hb2'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "0004" then
+		yhnames = '80hb3'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "0005" then
+		yhnames = '80hb4'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "0006" then
+		yhnames = '80hb5'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "0007" then
+		yhnames = '80hb6'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "0009" then
+		yhnames = '80hb7'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "0010" then
+		yhnames = '80hb8'
+		fuwuqi = '114.104.210.220'
+	end	
+--1-10		
+	if device.name() == "iPhone01" then
+		yhnames = '40vpn0'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone02" then
+		yhnames = '40vpn1'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone03" then
+		yhnames = '40vpn2'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone04" then
+		yhnames = '40vpn3'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone05" then
+		yhnames = '40vpn4'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone06" then
+		yhnames = '40vpn5'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone07" then
+		yhnames = '40vpn6'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone09" then
+		yhnames = '40vpn7'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone10" then
+		yhnames = '40vpn0'
+		fuwuqi = '114.104.210.220' 
+end	
+--11-20
+	if device.name() == "iPhone14" then
+		yhnames = '20hb0'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone15" then
+		yhnames = '20hb1'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone16" then
+		yhnames = '20hb2'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone17" then
+		yhnames = '20hb3'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone18" then
+		yhnames = '20hb0'
+		fuwuqi = '114.104.210.220'  
+	elseif device.name() == "iPhone19" then
+		yhnames = '20hb1'
+		fuwuqi = '114.104.210.220'  
+	elseif device.name() == "iPhone20" then
+		yhnames = '20hb2'
+		fuwuqi = '114.104.210.220' 
+end	
+--46-60
+	if device.name() == "iPhone46" then
+		yhnames = 'a001hb0'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone47" then
+		yhnames = 'a001hb1'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone48" then
+		yhnames = 'a001hb2'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone49" then
+		yhnames = 'a001hb3'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone50" then
+		yhnames = 'a001hb4'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone56" then
+		yhnames = 'a001hb5'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone57" then
+		yhnames = 'a001hb6'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone58" then
+		yhnames = 'a001hb7'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone59" then
+		yhnames = 'a001hb8'
+		fuwuqi = '114.104.176.29' 
+end	
+
+--21-40
+	if device.name() == "iPhone21" then
+		yhnames = '40vpn0'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone22" then
+		yhnames = '40vpn1'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone24" then
+		yhnames = '40vpn2'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone25" then
+		yhnames = '40vpn3'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone26" then
+		yhnames = '40vpn4'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone28" then
+		yhnames = '40vpn5'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone30" then
+		yhnames = '40vpn6'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone31" then
+		yhnames = '40vpn7'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone32" then
+		yhnames = '40vpn0'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone34" then
+		yhnames = '40vpn1'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone35" then
+		yhnames = '40vpn2'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone36" then
+		yhnames = '40vpn3'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone37" then
+		yhnames = '40vpn4'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone38" then
+		yhnames = '40vpn5'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone39" then
+		yhnames = '40vpn6'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone40" then
+		yhnames = '40vpn7'
+		fuwuqi = '114.104.210.220' 
+
+	end	
+
+--61-80
+	if device.name() == "iPhone61" then
+		yhnames = '80hb0'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone62" then
+		yhnames = '80hb1'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone63" then
+		yhnames = '80hb2'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone64" then
+		yhnames = '80hb3'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone65" then
+		yhnames = '80hb4'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone66" then
+		yhnames = '80hb5'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone67" then
+		yhnames = '80hb6'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone68" then
+		yhnames = '80hb7'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone69" then
+		yhnames = '80hb8'
+		fuwuqi = '114.104.176.29'  
+	elseif device.name() == "iPhone70" then
+		yhnames = '80hb0'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone71" then
+		yhnames = '80hb1'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone72" then
+		yhnames = '80hb2'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone74" then
+		yhnames = '80hb3'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone75" then
+		yhnames = '80hb4'
+		fuwuqi = '114.104.210.220'
+	elseif device.name() == "iPhone76" then
+		yhnames = '80hb5'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone77" then
+		yhnames = '80hb6'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone78" then
+		yhnames = '80hb7'
+		fuwuqi = '114.104.210.220' 
+	elseif device.name() == "iPhone79" then
+		yhnames = '80hb8'
+		fuwuqi = '114.104.210.220'
+		
+	end	
+
+----创建一个vpn
+yhnames = 'cradc'
+--fuwuqi = '114.104.176.29'  --s1全国
+--fuwuqi = '114.104.210.220'  --s2全国	
+--fuwuqi = 'yhtip.com'  
+fuwuqi = 'a05.cndtip.com'  
+vpnlist ={
+		dispName = yhnames,
+		VPNType = "L2TP",
+		server = fuwuqi ,
+		authorization = yhnames,
+		password = 'cradc',           -- 密码
+		secret = '888888',            -- 密钥，PPTP 可不填
+		encrypLevel = 1,               -- 加密级别，选填，默认 1
+		group = '',                    -- 群组名称，选填，默认 ""
+		VPNSendAllTraffic = 1,         -- 是否发送所有流量，选填，默认 1
+	}
+
+function creat_vpn(table)
+	local success = vpnconf.create{
+		dispName = table.dispName,
+		VPNType = table.VPNType,
+		server = table.server,
+		authorization = table.authorization,
+		password = table.password,
+		secret = table.secret,
+		encrypLevel = table.encrypLevel,
+		group = table.group,
+		VPNSendAllTraffic = table.VPNSendAllTraffic,
+	}
+	if success then
+		return true
+	else
+		sys.toast('创建失败，确定人品没有问题？')
+	end
+end
+
+
+creat_vpn(vpnlist)
+delay(5)
+
+-------选择一个VPN
+local success = vpnconf.select(yhnames)
+if success then
+    sys.alert('操作成功')
+else
+    sys.alert('操作失败，确认你要选中的 VPN 配置存在？')
+end
+--]]
+
+-----删除所有VPN
+
+
+--local vpnlist = vpnconf.list()
+--if vpnlist then
+--    for _,v in ipairs(vpnlist) do
+--        if (vpnconf.delete(v.VPNID)) then
+--            sys.log('成功删除：'..v.dispName..'('..v.VPNID..')')
+--        else
+--            sys.log('无法删除：'..v.dispName..'('..v.VPNID..')')
+--        end
+--    end
+--    sys.alert('操作完成！')
+--else
+--    sys.alert('获取列表失败，确定人品没有问题？')
+--end
+
+
+
+
+
+
+
+
+
+--log(app.bundles())
+
+--log(app.localized_name(app.front_bid()))
+
+
+--log(app.front_bid())
+--app.uninstall('com.apple.calculator')
+
+--[[
+
+applist = app.bundles()
+
+
+ key = 1
+for k = 1,#applist do
+	if app.localized_name(applist[key]) == 'X.X.T.' or app.localized_name(applist[key]) == 'Cydia' or app.localized_name(applist[key]) =='Electra' then
+		sys.toast('主要文件跳过')
+		log('主要文件跳过')
+		key = key +1
+	elseif app.localized_name(applist[key]) then
+		log('正在删除'..app.localized_name(applist[key]),true)
+		if app.uninstall(applist[key]) then
+			delay(10)
+			
+		end
+		key = key + 1
+	end
+	delay(1)
+end	
+sys.alert('删除完成')
+--sys.alert('删除完成'..sys.free_disk_space())
+os.exit()
+--]]--
+
+
+--for k = 1, 50 do
+	
+--	nLog('18:'..math.random(12,20)..':'..math.random(1,60))
+	
+--end	
+
+
+--[[
+
+while true do
+moveTo(322, 830,447, 195,5,1)
+delay(25)
+end
+--while true do
+
+--touch.on(405, 201):move(405, 201):off()
+
+--delay(2)
+--end
+
+
+--]]
+
+
+--app.quit(app.front_bid())
+
+
+--closeX('com.apple.AppStore')
+--delay(3)
+--active('com.apple.AppStore')
+--delay(7)
+--click(579, 1094)
+--delay(3)
+--click(232, 311)
+--input('上海证券指e通')
+--click(543, 1104)
+
+
+
+
+--openUrl("https://passport.qpgame.com/user/register.aspx")
+
+
+
+
+
+
+
+
+
+
